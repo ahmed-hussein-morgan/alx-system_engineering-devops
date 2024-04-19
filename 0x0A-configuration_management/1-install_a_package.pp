042033b8234401926e10956f7f75372d563927a1
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-04-19 23:24:20 +0200

    task 01

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,13 @@
+# Using Puppet, install flask from pip3.
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  name     => 'Werkzeug',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  name     => 'flask'
+}
